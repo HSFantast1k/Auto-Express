1a5d8e784dfa2778f17653afd92ea2743f3ff5c3
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-09-29 21:00:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ $(function(){
         arrows: false,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 
 });
